99d4efd8ab0d2f78d335dd1cf85078ba47955762
JuanU <juanu@fumoctl.com>
2026-08-10 08:34:43 -0300

    update and removed discover

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -145,6 +145,9 @@
   };
 
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    discover
+  ];
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
